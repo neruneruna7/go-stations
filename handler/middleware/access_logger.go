0eae52ec105aecb84127bbbb394a87561ed56140
neruneruna7 <neruneruna7@gmail>
2024-03-15 15:18:54 +0900

    add: Acces Logger middleware

---
+++ handler/middleware/access_logger.go
@@ -0,0 +1,58 @@
+package middleware
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type LogContent struct {
+	Timestamp time.Time `json:"timestamp"`
+	// 単位ミリ秒の処理時間
+	Latency int64  `json:"latency"`
+	Path    string `json:"path"`
+	Os      string `json:"os"`
+}
+
+// パニックハンドラ
+func AccessLogger(h http.Handler) http.Handler {
+
+	// 今回のコードだと，これはNewRouterが呼ばれた時に実行される
+	// クロージャ内はリクエストが来た時に動くことがわかるように，こういうロギングをつけてみた
+
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Closure AccessLogger Middlware started")
+
+		// アクセスしてきたときの時間を記録する
+		var start_time = time.Now()
+
+		defer func() {
+			log.Println("deferd AccessLogger Middlware started")
+			var end_time = time.Now()
+			var latency = end_time.Sub(start_time).Milliseconds()
+			var path = r.URL.Path
+			var os = r.Context().Value(CTX_OS_KEY).(string)
+
+			var log_content = &LogContent{
+				Timestamp: start_time,
+				Latency:   latency,
+				Path:      path,
+				Os:        os,
+			}
+
+			serialized, err := json.Marshal(log_content)
+			if err != nil {
+				log.Println(err)
+			}
+			fmt.Println(string(serialized))
+
+			log.Println("deferd AccessLogger Middlware finished")
+		}()
+
+		h.ServeHTTP(w, r)
+		log.Println("Closure AccessLogger Middlware finished")
+	}
+	return http.HandlerFunc(fn)
+}
--- handler/middleware/middlewares.go
+++ handler/middleware/middlewares.go
@@ -8,7 +8,10 @@ func Middlwares(h http.Handler) http.Handler {
 	// いちいちミドルウェアをすべて適用するのが面倒だから，まとめて適用する関数を作る
 
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		var middlewares_wraped_handler = Recovery(CaptureDeviceOs(h))
+		// deferはLIFOで積みあがっていくことに注意（スタック）
+		// Recoverを最初に入れる（deferは最後に実行する）にすることで，他のミドルウェア内のpanic
+		// も拾うことができるはず
+		var middlewares_wraped_handler = Recovery(CaptureDeviceOs(AccessLogger(h)))
 
 		middlewares_wraped_handler.ServeHTTP(w, r)
 	}
